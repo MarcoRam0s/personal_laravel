2e6e7bcbef01b0e099b0015fbefb18a59c62d9ba
Marco Antonio Ramos <94930318+MarcoRam0s@users.noreply.github.com>
2023-06-21 11:52:45 -0300

    Criado Controller de contato

---
+++ app/Http/Controllers/ContatoController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use PhpParser\Node\Stmt\Return_;
+
+class ContatoController extends Controller {
+    public function index() {
+        return "Contato Controller";
+    }
+}
